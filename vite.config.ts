9caed67c6d98d3230df1501de13424388aaa2161
Pawel <keniak313@gmail.com>
2026-04-14 14:18:51 +0200

    gsap no external

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit(), threlteStudio()],
 	ssr: {
-		noExternal: ['camera-controls', 'postprocessing']
+		noExternal: ['camera-controls', 'postprocessing', 'gsap']
 	}
 });
