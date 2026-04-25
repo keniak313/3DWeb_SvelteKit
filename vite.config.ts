3a6efe42478d53d10c34312549b72e664a8d0b21
Pawel <keniak313@gmail.com>
2026-04-25 12:41:12 +0200

    update to multiple three instances on server

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,16 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { threlteStudio } from '@threlte/studio/vite';
 import { defineConfig } from 'vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
+import path from 'path';
 
 export default defineConfig({
 	plugins: [sveltekit(), threlteStudio(), enhancedImages()],
 	resolve: {
 		alias: {
-			three: 'three'
+			three: path.resolve('./node_modules/three')
 		}
 	},
 	ssr: {
-		noExternal: ['camera-controls', 'postprocessing', 'gsap', 'three']
+		noExternal: ['camera-controls', 'postprocessing', 'three', '@threlte/core', '@threlte/extras']
 	}
 });
