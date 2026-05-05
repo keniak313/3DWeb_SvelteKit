3a41cd5525732f9a3b082c6a5def10934617bb3b
Pawel <keniak313@gmail.com>
2026-05-05 10:31:25 +0200

    icon size change

--- src/lib/components/Library.svelte
+++ src/lib/components/Library.svelte
@@ -27,7 +27,7 @@
 							src={model.icon}
 							updatedAt={model.updatedAt}
 							isNew={model.newIcon}
-							size={80}
+							size={128}
 						/>
 						<p>{model.name}</p>
 					</div>
@@ -42,7 +42,7 @@
 			{#if textures.length > 0}
 				{#each textures as texture (texture.id)}
 					<div class="item">
-						<ItemIcon src={texture.url} updatedAt={texture.updatedAt} size={80} />
+						<ItemIcon src={texture.url} updatedAt={texture.updatedAt} size={128} />
 						<p>{texture.name}</p>
 					</div>
 				{/each}
