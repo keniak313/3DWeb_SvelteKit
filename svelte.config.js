c70425d4d752dd493d78c658e035cd5c518291d3
Pawel <keniak313@gmail.com>
2026-05-05 10:17:22 +0200

    added vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,7 +10,14 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({
+			images: {
+				sizes: [64, 128, 256, 512, 1024, 2048],
+				formats: ['image/webp'],
+				minimumCacheTTL: 300,
+				domains: ['3d-web-svelte-kit.vercel.app']
+			}
+		}),
 		typescript: {
 			config: (config) => ({
 				...config,
